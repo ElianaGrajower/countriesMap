36b9939b60b6965dd9fa884096b7cf41c15e52f0
ElianaGrajower <92116878+ElianaGrajower@users.noreply.github.com>
2023-11-20 11:46:17 +0200

    fix

--- functions.js
+++ functions.js
@@ -2,7 +2,7 @@ const baseUrl = 'https://restcountries.com/v3.1/';
 
 const getCountry = async (country) => {
     try {
-        const res = await axios.get(`${baseUrl}name/${country}?fields=name,flags,ccn3`);
+        const res = await axios.get(`${baseUrl}name/${country}?fields=name,flags,cca3`);
         return res.data;
     } catch (error) {
         console.log(error);
